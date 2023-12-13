7e3364922634c3b16f897443d786fe2303d3dee3
Adryan Melo <adryanrodrigues42@gmail.com>
2023-12-13 14:06:31 -0300

    ad

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+    $('header button').click(function(){
+        $('form').slideDown();
 
+    })
+
+    $('#button_cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+$('form').on('submit',function(e){
+        console.log('submit');
+        e.preventDeFalt();
+})
+
+})
