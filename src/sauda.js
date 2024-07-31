07e04f9019242eeaabc05f081e0b5b0e5af3bbb7
Adryan-Melo <149001545+Adryan-Melo@users.noreply.github.com>
2024-07-31 17:15:14 -0300

    Delete src/sauda.js

--- src/sauda.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function saudacao(Nome) {
-    return `Olá ${Nome}, como vai?`;
-}
-;
-const SeuNome = "Maria";
-const saudacaoSeuNome = saudacao(SeuNome);
-console.log(saudacaoSeuNome);
